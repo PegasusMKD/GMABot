ae272d644cf4a7ea9ba253b50750a43d6e0af053
PegasusMKD <csprogrammers@outlook.com>
2021-11-30 02:59:05 +0100

    Implemented Horoscope slash commands, needs testing after commands are added

---
+++ Slash Commands/WeeklyHoroscope.cs
@@ -0,0 +1,26 @@
+﻿using GMABot.Factories;
+using GMABot.HTTP;
+using GMABot.Models.Schedules;
+using GMABot.Slash_Commands.Core;
+
+namespace GMABot.Slash_Commands
+{
+    [Subcommand(Name = "weekly", Group = "horoscope", Description = "Weekly horoscope info.")]
+    internal class WeeklyHoroscope : ISubcommand
+    {
+        public DiscordParameter[]? parameters => new DiscordParameter[] {
+                new DiscordParameter { name = "horoscope-type", description = "Which horoscope are you interested (\"skorpija\" for example).", required = true }
+            };
+
+        public void Execute(string channel, params object[] parameters)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                string horoscopeType = (parameters[0] as String)!;
+                Schedule schedule = new Schedule() { title = $"Horoscope - {horoscopeType}", type = FormatType.EMBED };
+                var message = DiscordMessageFactory.CreateMessage(schedule, HTMLParser.ParseHtmlText($"http://www.astromagazin.com.mk/{horoscopeType}/nedelen.php"));
+                DiscordHttpClient.SendMessage(message, channel);
+            });
+        }
+    }
+}
