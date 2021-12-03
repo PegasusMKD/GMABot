7c352a8f816de93d26e96b0a10f6f57f170c165b
PegasusMKD <csprogrammers@outlook.com>
2021-12-03 02:22:27 +0100

    Added extra outputs

--- activate.sh
+++ activate.sh
@@ -5,4 +5,4 @@ sudo git fetch --all
 sudo git reset --hard HEAD
 sudo git pull origin
 /home/pi/.dotnet/dotnet publish -c Release
-/home/pi/.dotnet/dotnet ./bin/Release/net6.0/publish/GMABot.dll
+/home/pi/.dotnet/dotnet ./bin/Release/net6.0/publish/GMABot.dll > /home/pi/logs/dotnet/dotnet.log 2>/home/pi/logs/dotnet/dotnet-errors.log
